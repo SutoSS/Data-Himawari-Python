fd50837ad0bd1cac32b86495f8d23da173044388
Suto Setiyadi <67867879+SutoSS@users.noreply.github.com>
2023-03-25 08:07:11 +0700

    Update Training_data.py

--- Training_data.py
+++ Training_data.py
@@ -1,41 +1,195 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from keras.layers import Dense, BatchNormalization, Dropout, LSTM
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+from keras import callbacks
+import pandas as pd
+import numpy as np
 
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Input, Dropout
-from sklearn.metrics import accuracy_score
-
-#Load Dataset
+#Load Data
 data = pd.read_csv("olahdata1.csv")
-data = data.astype('float32')
-data = data.to_numpy()
 
-#Select Data Training and Testing
-training = data[0:130]
-testing = data[130:]
+#first of all let us evaluate the target and find out if our data is imbalanced or not
+cols= ["#C2C4E2","#EED4E5"]
+sns.countplot(x= data['label'], palette= cols)
+plt.show()
 
-#Select Coloum Array
-training_features = training[:,1:-1]
-training_labels = training[:,0]
-testing_features = testing[:,1:-1]
-testing_labels = testing[:,0]
+#Parsing datetime
+#exploring the length of date objects
+lengths = data['date'].str.len()
+lengths.value_counts()
 
-#Show shape input
-shape_value = training_features[0].shape
-print(shape_value)
+data['Date']= pd.to_datetime(data['date'])
+#Creating a collumn of year
+data['year'] = data.Date.dt.year
 
-#Build the model
+# function to encode datetime into cyclic parameters. 
+#As I am planning to use this data in a neural network I prefer the months and days in a cyclic continuous feature. 
+
+def encode(data, col, max_val):
+    data[col + '_sin'] = np.sin(2 * np.pi * data[col]/max_val)
+    data[col + '_cos'] = np.cos(2 * np.pi * data[col]/max_val)
+    return data
+
+data['month'] = data.Date.dt.month
+data = encode(data, 'month', 12)
+
+data['day'] = data.Date.dt.day
+data = encode(data, 'day', 31)
+
+# roughly a year's span section 
+section = data[:140] 
+tm = section["day"].plot(color="#C2C4E2")
+tm.set_title("Distribution Of Days Over Year")
+tm.set_ylabel("Days In month")
+tm.set_xlabel("Days In Year")
+plt.show()
+
+cyclic_month = sns.scatterplot(x="month_sin",y="month_cos",data=data, color="#C2C4E2")
+cyclic_month.set_title("Cyclic Encoding of Month")
+cyclic_month.set_ylabel("Cosine Encoded Months")
+cyclic_month.set_xlabel("Sine Encoded Months")
+plt.show()
+
+cyclic_day = sns.scatterplot(x='day_sin',y='day_cos',data=data, color="#C2C4E2")
+cyclic_day.set_title("Cyclic Encoding of Day")
+cyclic_day.set_ylabel("Cosine Encoded Day")
+cyclic_day.set_xlabel("Sine Encoded Day")
+plt.show()
+
+# Get list of categorical variables
+s = (data.dtypes == "object")
+object_cols = list(s[s].index)
+
+print("Categorical variables:")
+print(object_cols)
+
+# Missing values in categorical variables
+for i in object_cols:
+    print(i, data[i].isnull().sum())
+
+# Filling missing values with mode of the column in value
+for i in object_cols:
+    data[i].fillna(data[i].mode()[0], inplace=True)
+    
+# Get list of neumeric variables
+t = (data.dtypes == "float64")
+num_cols = list(t[t].index)
+
+print("Neumeric variables:")
+print(num_cols)
+
+# Missing values in numeric variables
+for i in num_cols:
+    print(i, data[i].isnull().sum())
+
+# Filling missing values with median of the column in value
+for i in num_cols:
+    data[i].fillna(data[i].median(), inplace=True)
+    
+# Apply label encoder to each column with categorical data
+label_encoder = LabelEncoder()
+for i in object_cols:
+    data[i] = label_encoder.fit_transform(data[i])
+
+# Prepairing attributes of scale data
+features = data.drop(['label', 'Date','day', 'month'], axis=1) # dropping target and extra columns
+target = data['label']
+
+#Set up a standard scaler for the features
+col_names = list(features.columns)
+s_scaler = preprocessing.StandardScaler()
+features = s_scaler.fit_transform(features)
+features = pd.DataFrame(features, columns=col_names) 
+
+#Detecting outliers
+#looking at the scaled features
+colours = ["#D0DBEE", "#C2C4E2", "#EED4E5", "#D1E6DC", "#BDE2E2"]
+plt.figure(figsize=(30,20))
+sns.boxenplot(data = features,palette = colours)
+plt.xticks(rotation=90)
+plt.show()
+
+#full data for 
+features['label'] = target
+X = features.drop(['label'], axis=1)
+y = features['label']
+print(y)
+
+# Splitting test and training sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+print(X.shape)
+
+#Early stopping
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=20, # how many epochs to wait before stopping
+    restore_best_weights=True,
+)
+
+#Initialising the NN
 model = Sequential()
-model.add(Input(shape=(5,)))
-model.add(Dense(1, activation = 'relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation = 'relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation = 'sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
-model.fit(training_features, training_labels, epochs=50, validation_data = (testing_features,testing_labels))
+#layers
+model.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu', input_dim = 12))
+model.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.25))
+model.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
-y_hat = model.predict(testing_features)
-print(y_hat)
+# # Build the LSTM model
+# model = Sequential()
+# model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+# model.add(LSTM(64, return_sequences=False))
+# model.add(Dense(25))
+# model.add(Dense(1))
+
+#Compiling
+opt = Adam(learning_rate=0.00009)
+model.compile(optimizer = opt, loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Train
+history = model.fit(X_train, y_train, batch_size = 32, epochs = 150, callbacks=[early_stopping], validation_split=0.2)
+
+history_df = pd.DataFrame(history.history)
+plt.plot(history_df.loc[:, ['loss']], "#BDE2E2", label='Training loss')
+plt.plot(history_df.loc[:, ['val_loss']],"#C2C4E2", label='Validation loss')
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(loc="best")
+plt.show()
+
+history_df = pd.DataFrame(history.history)
+plt.plot(history_df.loc[:, ['accuracy']], "#BDE2E2", label='Training accuracy')
+plt.plot(history_df.loc[:, ['val_accuracy']], "#C2C4E2", label='Validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Predicting the test set results
+y_pred = model.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# confusion matrix
+cmap1 = sns.diverging_palette(260,-10,s=50, l=75, n=5, as_cmap=True)
+plt.subplots(figsize=(12,8))
+cf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(cf_matrix/np.sum(cf_matrix), cmap = cmap1, annot = True, annot_kws = {'size':15})
+plt.show()
+
+print(classification_report(y_test, y_pred))
